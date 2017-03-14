7ea5983254bd7873d74e0d5a933fbdb634a24727
JaviAZ <javialcazafra@gmail.com>
2017-03-14 19:42:45 +0000

    Created main method and the methods this should lead to and the sub methods the admin method should lead to. Can be runned with no errors.

--- tms.py
+++ tms.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python3
-
+from database import *
 
 def viewTutorList():
+	print("To be completed")
 
-
-def editTutor(tutorID):
-
+def editTutor(tutorObj):
+	editChoice=input("Do you wish to edit name, surname, email, or group?").lower()
+	if editChoice=="name":
+		print("To be completed")
+	elif editChoice=="surname":
+		print("To be completed")
+	elif editChoice=="email":
+		print("To be completed")
+	elif editChoice=="group":
+		print("To be completed")
+	else:
+		print("Please input name, surname, email or group.")
 
 def viewStudentList():
+	print("To be completed")
 
-
-def editStudent(studentNumb):
-
+def editStudent(studentObj):
+	print("To be completed")
 
 def createProgram():
-
+	print("To be completed")
 
 def startProgram():
-
+	print("To be completed")
 
 def endProgram():
-
+	print("To be completed")
 
 def admin():
 	print("Manage tutors:                             Manage students:")
@@ -38,45 +48,64 @@ def admin():
 		viewTutorList()
 	elif adminOption=="edit tutors":
 		tflag=True
+		tflag2=True
 		while tflag:
-			tutorSurname=input("Input the name of the tutor you wish to edit: ")
+			tutorSurname=input("Input the surname of the tutor you wish to edit: ").lower()
 			tcount=0
-			tscount=0
-			for tutorSrnm in tutorList:
-				ts++
-				if tutorSurname==tutorSrnm["surname"]:
-					tcount++
+			for tutorKey in tutorList:
+				if tutorSurname==tutorList[tutorKey]["surname"].lower():
+					tcount+=1
 					tflag=False
-					print ("ID: "+ts+". Name: "+tutorList[ts]["name"]+". Surname: "+tutorSrnm+". Email: "+tutorList[ts]["email"])
+					print ("ID: "+tutorList[tutorKey]["id"]+". Name: "+tutorList[tutorKey]["name"]+". Surname: "+tutorList[tutorKey]["surname"]+". Email: "+tutorList[tutorKey]["email"])
 			if tcount==0:
 				print("Sorry the tutor was not found.")
 			else:
-				tChoice=int(input("Enter the ID of the tutor you wish to edit: "))
-				if tChoice>tutorList.size() or tChoice<0:
-					print("That tutor isn't in the list.")
-				else:
-					editTutor(tChoice)
-
-
-
+				while tflag2:
+					tChoice=input("Enter the ID of the tutor you wish to edit: ")
+					if tChoice in tutorList:
+						editTutor(tutorList[tChoice])
+						tflag2=False
+						break
+					else:
+						print("That tutor isn't in the list.")
 	elif adminOption=="view students":
-
+		viewStudentList()
 	elif adminOption=="edit students":
-
+		sflag=True
+		sflag2=True
+		while sflag:
+			studentSurname=input("Input the surname of the student you wish to edit: ").lower()
+			scount=0
+			for studentKey in studentList:
+				if studentSurname==studentList[studentKey]["surname"].lower():
+					scount+=1
+					sflag=False
+					print ("Student number: "+studentList[studentKey]["studentNo"]+". Name: "+studentList[studentKey]["name"]+". Surname: "+studentList[studentKey]["surname"]+". Email: "+studentList[studentKey]["email"])
+			if tcount==0:
+				print("Sorry the student was not found.")
+			else:
+				while sflag2:
+					Choice=input("Enter the student number of the student you wish to edit: ")
+					if sChoice in studentList:
+						editTutor(studentList[sChoice])
+						sflag2=False
+						break
+					else:
+						print("That student isn't in the list.")
 	elif adminOption=="create program":
-
+		createProgram()
 	elif adminOption=="start program":
-
+		startProgram()
 	elif adminOption=="end program":
-
+		endProgram()
 	else:
 		print("Please input view tutors, edit tutors, view students, edit students, create program, start program or end program.")
 
 def tutor():
-
+	print("To be completed")
 
 def student():
-
+	print("To be completed")
 
 def main():
 	print("Cardiff University Computer Science Tutor Management System")
@@ -93,3 +122,5 @@ def main():
 		student()
 	else:
 		print("Please input admin, tutor or student.")
+
+main()
