c95a1f09d95dc536d066d8357d2e13449eb4d921
JaviAZ <javialcazafra@gmail.com>
2017-03-14 19:45:28 +0000

    Created a template for student, tutor, studentList, tutorList and tutorGroup. For testing purposes.

---
+++ database.py
@@ -0,0 +1,27 @@
+#All data in this file is for template and testing. in the final version, dictionaries will be empty, and when created will follow this pattern.
+
+student={
+	"studentNo":"C1628112",
+	"name":"Javier",
+	"surname":"Alcazar",
+	"email":"AlcazarZafraJ@cardiff.ac.uk",
+	"course":"UFBSCMSA",
+	"courseY":"2016/17",
+	"tutor":"1"
+}
+tutorGroup={
+	"C1628112":student
+}
+tutor={
+	"id":"1",
+	"name":"David",
+	"surname":"Marshall",
+	"email":"MarshallAD@cardiff.ac.uk",
+	"group":tutorGroup
+}
+studentList={
+	"C1628112":student
+}
+tutorList={
+	"1":tutor
+}
