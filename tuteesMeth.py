f07a6e1a03a75d61db2ad14497cd94e7068c9e57
AdamsKS <AdamsKS@cardiff.ac.uk>
2017-03-20 16:59:42 +0000

    Added viewTuteesList with minimal student info and a way to add new students

--- tuteesMeth.py
+++ tuteesMeth.py
@@ -5,14 +5,69 @@ import tms
 def addTutee(studentCode,surname,name,tutor,course,acadYear,email):	#will call addToGroup if method isnt called from createProgram
 	database.tuteeList[studentCode]={"tuteeNo":studentCode, "name":name, "surname":surname, "email":email, "course":course, "courseY":acadYear, "tutor":tutor}
 
-def removeTutee(): #remove from tutor group too 
+def removeTutee(surname): #remove from tutor group too 
+	#tuteeList=database.tuteeList
+	#for entry in tuteeList:
+	#	if entry["surname"] == surname:
+	#		print("Do you want to remove: " + surname)
+	#	else:
+	#		print("No Tutee's with the surname: " + surname)
 	print("To be completed")
 
 def viewTutorGroup():	#Print tutorgroup depending on tutor
 	print("To be completed")
 
 def viewTuteeList(): #Print tuteeList with option to add or remove tutees (which would call respective methods)
-	print("To be completed")
+	tuteeList=database.tuteeList
+	outputList = []
+	for entry in tuteeList:
+		temp = "	Name: " + tuteeList[entry]["name"] + " " + tuteeList[entry]["surname"]
+		outputList.append(temp)
+	for entry in outputList:
+		print(entry)
+	takeAction = True
+	undecided = True
+	while takeAction:
+		print("\n	Add student 		Remove Student 			Return to Menu")
+		action = tms.userInput("")	
+		if action.lower()=="add student":
+			print("What is the new students, student number: ")
+			studentCode=tms.userInput("")
+			print("And their first name: ")
+			name=tms.userInput("")
+			print("And their surname: ")
+			surname=tms.userInput("")
+			print("And their e-mail: ")
+			email=tms.userInput("")
+			print("And the course they're studying: ")
+			course=tms.userInput("")
+			print("And which year of study are they in: ")
+			courseY=tms.userInput("")
+			print("And who will be their tutor: ")
+			tutor=tms.userInput("")
+			print("So, \nStudent number: "+studentCode+"\nFullname: "+name+" "+surname+"\nE-mail: "+email+" \nCourse: "+course+"\nYear of study: "+courseY+"\nTutor: "+tutor) 
+			undecided = True
+			while undecided:
+				print("Is that correct: yes/no")
+				response = tms.userInput("")
+				if response=="yes":
+					takeAction=False
+					undecided=False
+					addTutee(studentCode, surname, name, email, course, courseY, tutor)
+					viewTuteeList()
+				elif response=="no":
+					print("You'll be returned to the menu to try again.")
+					undecided=False
+				else:
+					print("Please enter 'yes' or 'no'")
+		elif action.lower()=="remove student":
+			takeAction=False
+			removeTutee()
+		elif action.lower()=="return to menu":
+			takeAction=False
+			tms.admin()
+		else:
+			print("invalid input")
 
 def editTutee(): #Ask for tutee surname. show possible options or print error message. ask user for attribute to change.
 	sflag=True
